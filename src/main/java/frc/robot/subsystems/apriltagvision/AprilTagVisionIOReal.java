508dadf6ff4c8f6056156895153274f649969969
FlameFish <mccoa499@gmail.com>
2025-01-09 11:29:02 -0800

    Made stub for AprilTagVisionIOReal

--- src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIOReal.java
+++ src/main/java/frc/robot/subsystems/apriltagvision/AprilTagVisionIOReal.java
@@ -1,7 +1,18 @@
 package frc.robot.subsystems.apriltagvision;
 
-import frc.robot.Constants.AprilTagConstants;
+import frc.robot.Constants.AprilTagConstants.*;
 
 public class AprilTagVisionIOReal implements AprilTagVisionIO {
+  public AprilTagVisionIOReal() {
     
+  }
+
+  @Override
+  public void updateInputs(AprilTagVisionIOInputs inputs) {
+
+  }
+
+  public void getEstimatedPoseUpdates() {
+    
+  }
 }

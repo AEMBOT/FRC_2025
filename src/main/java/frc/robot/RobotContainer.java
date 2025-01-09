62687939f6dec18253b4d833e966173d7a5ef52d
FlameFish <mccoa499@gmail.com>
2025-01-08 18:43:48 -0800

    Switched to using on-the-fly manual path generation instead of AD* pathfinding for going to source.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,23 @@
 
 package frc.robot;
 
+import static edu.wpi.first.units.Units.Degrees;
+import static edu.wpi.first.units.Units.Radians;
+
 import java.io.IOException;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
+import com.pathplanner.lib.path.Waypoint;
 import com.pathplanner.lib.util.FileVersionException;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.subsystems.drive.Drive;
@@ -28,19 +36,22 @@ public class RobotContainer {
 
   private void configureBindings() {
     // Auto/drive assist stuff
-    PathPlannerPath alignToSourcePath = null;
-    try {
-      alignToSourcePath = PathPlannerPath.fromPathFile("Go to source");
-    } catch (FileVersionException | IOException | ParseException e) {
-      e.printStackTrace();
-    }
-    PathConstraints alignToSourceConstraints = new PathConstraints(
-      0.5, 
-      0.1, 
-      0.1, 
-      0.1
+    List<Waypoint> sourceNavigationWaypoints = PathPlannerPath.waypointsFromPoses(
+        new Pose2d(1.196, 1.092, Rotation2d.fromDegrees(0))
     );
-    Command alignToSource = AutoBuilder.pathfindThenFollowPath(alignToSourcePath, alignToSourceConstraints);
+
+    PathConstraints sourceNavigationConstraints = new PathConstraints(
+      3.0, 3.0, 
+      Radians.convertFrom(45, Degrees), Radians.convertFrom(45, Degrees)
+    ); // The constraints for this path.
+
+    // Create the path using the waypoints created above
+    PathPlannerPath sourceNavigationPath = new PathPlannerPath(
+            sourceNavigationWaypoints,
+            sourceNavigationConstraints,
+            null, // The ideal starting state, this is only relevant for pre-planned paths, so can be null for on-the-fly paths.
+            new GoalEndState(0.0, Rotation2d.fromDegrees(-125)) // Goal end state. You can set a holonomic rotation here. If using a differential drivetrain, the rotation will have no effect.
+    );
   }
 
   public Command getAutonomousCommand() {

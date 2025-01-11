4b1cb93c0a4f093738e5435d74256658203ba454
Ace5tar <tace233@gmail.com>
2025-01-11 15:07:45 -0800

    copied bunnybots drive code and updated to 2025

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.Logger;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -15,7 +17,11 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+
     m_robotContainer = new RobotContainer();
+
+    // Start AdvantageKit logger
+    Logger.start();
   }
 
   @Override
---
+++ src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
@@ -0,0 +1,66 @@
+// Copyright 2021-2024 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
+package frc.robot.subsystems.drive;
+
+import static frc.robot.Constants.currentRobot;
+import static frc.robot.Constants.DriveConstants.Module.ODOMETRY_FREQUENCY;
+
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.SPI;
+import java.util.Queue;
+import org.littletonrobotics.junction.Logger;
+
+/** IO implementation for NavX */
+public class GyroIONavX implements GyroIO {
+  private final AHRS navX = new AHRS(SPI.Port.kMXP, (byte) ODOMETRY_FREQUENCY); // 200Hz update rate
+  private final Queue<Double> yawPositionQueue;
+  private final Queue<Double> yawTimestampQueue;
+
+  public GyroIONavX() {
+    navX.reset();
+    yawPositionQueue = SparkMaxOdometryThread.getInstance().registerSignal(() -> navX.getAngle());
+    yawTimestampQueue =
+        SparkMaxOdometryThread.getInstance().registerSignal(() -> navX.getLastSensorTimestamp());
+  }
+
+  @Override
+  public void updateInputs(GyroIOInputs inputs) {
+    inputs.connected = navX.isConnected();
+    inputs.yawPosition = Rotation2d.fromDegrees(navX.getAngle());
+    inputs.yawVelocityRadPerSec = Units.degreesToRadians(navX.getRate());
+
+    inputs.odometryYawTimestamps =
+        yawTimestampQueue.stream().mapToDouble((Double value) -> value).toArray();
+    inputs.odometryYawPositions =
+        yawPositionQueue.stream()
+            // NavX is normally CW+ so this should be wrong
+            // But this fixes the drivetrain
+            // I believe the problem is that the NavX has not been correctly
+            // TODO NavX OMNIMOUNT configure both bots
+            .map(
+                (Double value) ->
+                Rotation2d.fromDegrees(
+                  switch (currentRobot) {
+                    case BUNNYBOT -> value; // According to last years code, clef NavX is inverted
+                    case LIGHTCYCLE -> -value;
+                })).toArray(Rotation2d[]::new);
+    yawTimestampQueue.clear();
+    yawPositionQueue.clear();
+
+    Logger.recordOutput("NavX Rotation", navX.getRotation2d());
+    Logger.recordOutput("NavX Last Sensor Timestamp", navX.getLastSensorTimestamp());
+  }
+}

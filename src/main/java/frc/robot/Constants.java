e91da01d97a5514a18a3f731e8d1585c3348b293
FlameFish <mccoa499@gmail.com>
2025-01-16 17:33:46 -0800

    Minor formatting changes on Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,7 +19,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 
-
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -28,79 +27,79 @@ import edu.wpi.first.math.numbers.N3;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants{
-  public static final DigitalInput robotJumper = new DigitalInput(0);
-  public static final Robot currentRobot = Robot.BUNNYBOT;
-  public static final Mode currentMode = Mode.REAL;
+public final class Constants {
+    public static final DigitalInput robotJumper = new DigitalInput(0);
+    public static final Robot currentRobot = Robot.BUNNYBOT;
+    public static final Mode currentMode = Mode.REAL;
 
-  public static enum Mode {
-    /** Running on a real robot. */
-    REAL,
+    public static enum Mode {
+        /** Running on a real robot. */
+        REAL,
 
-    /** Running a physics simulator. */
-    SIM,
+        /** Running a physics simulator. */
+        SIM,
 
-    /** Replaying from a log file. */
-    REPLAY
-  }
-
-  public enum Robot {
-    BUNNYBOT,
-    LIGHTCYCLE
-  }
-
-  public static final double UPDATE_PERIOD = 0.02;
-
-  public static final class DriveConstants {
-    // May need tweaking
-    public static final double MAX_LINEAR_SPEED = Units.feetToMeters(18.5); // MK4i L3+
-    public static final double TRACK_WIDTH_X = Units.inchesToMeters(22.75); // 28 in square chassis
-    public static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.75);
-    public static final double DRIVE_BASE_RADIUS =
-      Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
-    public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
-
-    public static final double CONTROLLER_DEADBAND = 0.05;
-    public static final double SLOWMODE_MAX_METERS_PER_SEC = 1;
-    public static final double SLOWMODE_ROTATION_SPEED_FACTOR = 0.2;
-
-    public static final class Module {
-      /* PORTS */
-      public static final int TALON_DRIVE_MOTOR_0 = 7;
-      public static final int TALON_TURN_MOTOR_0 = 8;
-      public static final int TALON_CANCODER_0 = 26;
-
-      public static final int TALON_DRIVE_MOTOR_1 = 5;
-      public static final int TALON_TURN_MOTOR_1 = 6;
-      public static final int TALON_CANCODER_1 = 24;
-
-      public static final int TALON_DRIVE_MOTOR_2 = 3;
-      public static final int TALON_TURN_MOTOR_2 = 4;
-      public static final int TALON_CANCODER_2 = 25;
-
-      public static final int TALON_DRIVE_MOTOR_3 = 9;
-      public static final int TALON_TURN_MOTOR_3 = 2;
-      public static final int TALON_CANCODER_3 = 23;
-
-      public static final double WHEEL_RADIUS = Units.inchesToMeters(1.906);
-      public static final double ODOMETRY_FREQUENCY = 200.0; // default 250, limited to 200 by NavX
-
-      public static final Rotation2d[] absoluteEncoderOffset = switch (currentRobot) {
-        case BUNNYBOT -> new Rotation2d[] {
-          Rotation2d.fromRadians(2.291767297101148), // FL
-          Rotation2d.fromRadians(2.409883817768342 + Math.PI), // FR
-          Rotation2d.fromRadians(1.928213850372251), // BL
-          Rotation2d.fromRadians(1.73493227109866 + Math.PI) // BR
-        };
-        case LIGHTCYCLE -> new Rotation2d[] { // This is not currently correct
-          Rotation2d.fromRadians(2.6599226861937018), // FL
-          Rotation2d.fromRadians(-2.9206994201342606 + Math.PI), // FR
-          Rotation2d.fromRadians(1.064582666792635), // BL
-          Rotation2d.fromRadians(-2.406815856192571 + Math.PI) // BR
-        };
-      };
+        /** Replaying from a log file. */
+        REPLAY
     }
-  }
+
+    public enum Robot {
+        BUNNYBOT,
+        LIGHTCYCLE
+    }
+
+    public static final double UPDATE_PERIOD = 0.02;
+
+    public static final class DriveConstants {
+        // May need tweaking
+        public static final double MAX_LINEAR_SPEED = Units.feetToMeters(18.5); // MK4i L3+
+        public static final double TRACK_WIDTH_X = Units.inchesToMeters(22.75); // 28 in square chassis
+        public static final double TRACK_WIDTH_Y = Units.inchesToMeters(22.75);
+        public static final double DRIVE_BASE_RADIUS =
+            Math.hypot(TRACK_WIDTH_X / 2.0, TRACK_WIDTH_Y / 2.0);
+        public static final double MAX_ANGULAR_SPEED = MAX_LINEAR_SPEED / DRIVE_BASE_RADIUS;
+
+        public static final double CONTROLLER_DEADBAND = 0.05;
+        public static final double SLOWMODE_MAX_METERS_PER_SEC = 1;
+        public static final double SLOWMODE_ROTATION_SPEED_FACTOR = 0.2;
+
+        public static final class Module {
+            /* PORTS */
+            public static final int TALON_DRIVE_MOTOR_0 = 7;
+            public static final int TALON_TURN_MOTOR_0 = 8;
+            public static final int TALON_CANCODER_0 = 26;
+
+            public static final int TALON_DRIVE_MOTOR_1 = 5;
+            public static final int TALON_TURN_MOTOR_1 = 6;
+            public static final int TALON_CANCODER_1 = 24;
+
+            public static final int TALON_DRIVE_MOTOR_2 = 3;
+            public static final int TALON_TURN_MOTOR_2 = 4;
+            public static final int TALON_CANCODER_2 = 25;
+
+            public static final int TALON_DRIVE_MOTOR_3 = 9;
+            public static final int TALON_TURN_MOTOR_3 = 2;
+            public static final int TALON_CANCODER_3 = 23;
+
+            public static final double WHEEL_RADIUS = Units.inchesToMeters(1.906);
+            public static final double ODOMETRY_FREQUENCY = 200.0; // default 250, limited to 200 by NavX
+
+            public static final Rotation2d[] absoluteEncoderOffset = switch (currentRobot) {
+                case BUNNYBOT -> new Rotation2d[] {
+                    Rotation2d.fromRadians(2.291767297101148), // FL
+                    Rotation2d.fromRadians(2.409883817768342 + Math.PI), // FR
+                    Rotation2d.fromRadians(1.928213850372251), // BL
+                    Rotation2d.fromRadians(1.73493227109866 + Math.PI) // BR
+                };
+                case LIGHTCYCLE -> new Rotation2d[] { // This is not currently correct
+                    Rotation2d.fromRadians(2.6599226861937018), // FL
+                    Rotation2d.fromRadians(-2.9206994201342606 + Math.PI), // FR
+                    Rotation2d.fromRadians(1.064582666792635), // BL
+                    Rotation2d.fromRadians(-2.406815856192571 + Math.PI) // BR
+                };
+            };
+        }
+    }
 
     public static final class AprilTagConstants {
         public static final AprilTagFieldLayout aprilTagFieldLayout;
@@ -120,13 +119,14 @@ public final class Constants{
             HIGH_RES,
             NORMAL
         }
+
         public final class LightcycleCameras {
             public static final String frontCamName = "front";
             public static final Transform3d frontCamToRobot =
-            new Transform3d(
-                new Translation3d(
-                    Units.inchesToMeters(11.32), Units.inchesToMeters(7.08), Units.inchesToMeters(7.8)),
-                new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(-30), 0.0));
+                new Transform3d(
+                    new Translation3d(
+                        Units.inchesToMeters(11.32), Units.inchesToMeters(7.08), Units.inchesToMeters(7.8)),
+                    new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(-30), 0.0));
 
             public static final String leftCamName = "left";
             public static final Transform3d leftCamToRobot =
@@ -138,7 +138,7 @@ public final class Constants{
                             Units.inchesToMeters(10.58)),
                         new Rotation3d(0.0, Units.degreesToRadians(-23.5), Units.degreesToRadians(147)));
                     case LIGHTCYCLE -> new Transform3d(
-                     new Translation3d(
+                        new Translation3d(
                             Units.inchesToMeters(-12.01),
                             Units.inchesToMeters(11.65),
                             Units.inchesToMeters(10.58)),
@@ -146,7 +146,7 @@ public final class Constants{
                             Units.degreesToRadians(180),
                             Units.degreesToRadians(-23.5),
                             Units.degreesToRadians(147)));
-            };
+                };
 
             public static final String rightCamName = "right";
             public static final Transform3d rightCamToRobot =
@@ -166,9 +166,9 @@ public final class Constants{
                             Units.degreesToRadians(180),
                             Units.degreesToRadians(-23.5),
                             Units.degreesToRadians(-147)));
-            };
+                };
         }
-        
+
         public static final Matrix<N3, N1> highResSingleTagStdDev =
             VecBuilder.fill(0.4, 0.4, Double.MAX_VALUE);
         public static final Matrix<N3, N1> normalSingleTagStdDev =

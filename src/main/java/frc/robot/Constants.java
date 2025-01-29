ee32e7410f304a7e8f9ad3c3934dc44e1cafd1e6
Spades-of-Aces <emily1aslan@gmail.com>
2025-01-28 23:35:30 -0800

    moved constants for target displacement from the nearest april tag + for reef center to constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,5 +87,17 @@ public final class Constants {
                 };
           };
     }
+
+    public static final class reefTargetConstants {
+      // Defines new variables for the x/y translations for the target positions (currently at
+      // placeholders)
+      public static final double targetX = 0.5;
+      public static final double targetY = 0.75;
+      public static final double targetAngle = 0;
+
+      // Define reef centerpoints (blue alliance)
+      public static final double reefCenterX = 4.489323;
+      public static final double reefCenterY = 4.0259;
+    }
   }
 }
--- src/main/java/frc/robot/util/ReefTargets.java
+++ src/main/java/frc/robot/util/ReefTargets.java
@@ -9,29 +9,24 @@ import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import frc.robot.Constants.DriveConstants.reefTargetConstants;
 
 public final class ReefTargets {
 
   // Define AprilTag layout
-  final AprilTagFieldLayout aprilTagFieldLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
+  final AprilTagFieldLayout aprilTagFieldLayout =
+      AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape);
   final Pose2d[] targets = new Pose2d[12];
 
-  // Find reef centerpoints
-  double reefCenterX = (aprilTagFieldLayout.getTagPose(21).get().getX() + aprilTagFieldLayout.getTagPose(18).get().getX())/2;
-  double reefCenterY = aprilTagFieldLayout.getTagPose(18).get().getY();
-
-  public ReefTargets(){
-
-    // Defines new variables for the x/y translations for the target positions (currently at placeholders)
-    double targetX = 0.5;
-    double targetY = 0.75;
-    double targetAngle = 0;
+  public ReefTargets() {
 
     // Defines the transformation vector for a target position
-    Rotation2d targetThetaR = new Rotation2d(targetAngle);
-    Rotation2d targetThetaL = new Rotation2d(-targetAngle);
-    Transform2d targetR = new Transform2d(targetX,targetY,targetThetaR);
-    Transform2d targetL = new Transform2d(targetX,-targetY,targetThetaL);
+    Rotation2d targetThetaR = new Rotation2d(reefTargetConstants.targetAngle);
+    Rotation2d targetThetaL = new Rotation2d(-reefTargetConstants.targetAngle);
+    Transform2d targetR =
+        new Transform2d(reefTargetConstants.targetX, reefTargetConstants.targetY, targetThetaR);
+    Transform2d targetL =
+        new Transform2d(reefTargetConstants.targetX, -reefTargetConstants.targetY, targetThetaL);
 
     // Defines each target position based upon the transformation vector and appropriate apriltag
     // 17 -> 1,2; 18 -> 0,11; 19 -> 9,10; 20 -> 7,8; 21 -> 5,6; 22 -> 3,4
@@ -47,7 +42,6 @@ public final class ReefTargets {
     targets[9] = aprilTagFieldLayout.getTagPose(19).get().toPose2d().transformBy(targetR);
     targets[10] = aprilTagFieldLayout.getTagPose(19).get().toPose2d().transformBy(targetL);
     targets[11] = aprilTagFieldLayout.getTagPose(18).get().toPose2d().transformBy(targetR);
-  
   }
 
   public int findClosestReef(Pose2d robotCurrentPosition) {
@@ -57,15 +51,15 @@ public final class ReefTargets {
     double robotY = robotCurrentPosition.getY();
 
     // Find robot angle to reef and convert to a discrete "zone" value
-    double reefAngle = Math.atan2(robotY - reefCenterY,robotX - reefCenterX);
-    int reefZone = (int)Math.floor(reefAngle/(Math.PI/6)) + 6;  
+    double reefAngle =
+        Math.atan2(
+            robotY - reefTargetConstants.reefCenterY, robotX - reefTargetConstants.reefCenterX);
+    int reefZone = (int) Math.floor(reefAngle / (Math.PI / 6)) + 6;
     return reefZone;
-
   }
 
   public Pose2d findTarget(Pose2d robotCurrentPosition) {
 
     return targets[findClosestReef(robotCurrentPosition)];
-
   }
 }

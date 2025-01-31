4a53a7814e66d76c07cf79902971f4002cc37597
FlameFish <mccoa499@gmail.com>
2025-01-30 18:30:44 -0800

    Switched the axes on front camera offsets, changed camera offset variable names for clarity.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -190,22 +190,22 @@ public final class Constants {
     }
 
     public final class DoryCameras {
-      public static final String frontLeftCamName = "front-left";
-      public static final Transform3d frontLeftCamToRobot =
+      public static final String frontLeftName = "front-left";
+      public static final Transform3d frontLeftFromRobot =
           new Transform3d(
               new Translation3d(
-                  Units.inchesToMeters(7), Units.inchesToMeters(11), Units.inchesToMeters(4)),
+                  Units.inchesToMeters(11), Units.inchesToMeters(7), Units.inchesToMeters(4)),
               new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(-30), 0.0));
 
-      public static final String frontRightCamName = "front-right";
-      public static final Transform3d frontRightCamToRobot =
+      public static final String frontRightName = "front-right";
+      public static final Transform3d frontRightFromRobot =
           new Transform3d(
               new Translation3d(
-                  Units.inchesToMeters(7), Units.inchesToMeters(-11), Units.inchesToMeters(4)),
+                  Units.inchesToMeters(11), Units.inchesToMeters(-7), Units.inchesToMeters(4)),
               new Rotation3d(Units.degreesToRadians(180), Units.degreesToRadians(-30), 0.0));
 
-      public static final String backLeftCamName = "back-left";
-      public static final Transform3d backLeftCamToRobot =
+      public static final String backLeftName = "back-left";
+      public static final Transform3d backLeftFromRobot =
           new Transform3d(
               new Translation3d(
                   Units.inchesToMeters(-11), Units.inchesToMeters(11.5), Units.inchesToMeters(6)),
@@ -214,8 +214,8 @@ public final class Constants {
                   Units.degreesToRadians(-23.5),
                   Units.degreesToRadians(147)));
 
-      public static final String backRightCamName = "back-right";
-      public static final Transform3d backRightCamToRobot =
+      public static final String backRightName = "back-right";
+      public static final Transform3d backRightFromRobot =
           new Transform3d(
               new Translation3d(
                   Units.inchesToMeters(-11), Units.inchesToMeters(-11.5), Units.inchesToMeters(6)),

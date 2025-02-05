91908ec56933705642ea7abf037988706d210166
FlameFish <mccoa499@gmail.com>
2025-02-04 16:26:36 -0800

    Change internal robot names BUNNYBOT -> DORY and LIGHTCYCLE -> NAUTILUS

--- src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIONavX.java
@@ -54,9 +54,9 @@ public class GyroIONavX implements GyroIO {
                 (Double value) ->
                     Rotation2d.fromDegrees(
                         switch (currentRobot) {
-                          case BUNNYBOT ->
+                          case DORY ->
                               -value; // According to last years code, clef NavX is inverted
-                          case LIGHTCYCLE -> -value;
+                          case NAUTILUS -> -value;
                         }))
             .toArray(Rotation2d[]::new);
     yawTimestampQueue.clear();

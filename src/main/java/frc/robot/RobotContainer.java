96b99b51dc1525d74e4ac8e69efd45743303ef8a
AEMBOT6443 <robotsdontquit@gmail.com>
2025-02-10 19:58:36 -0800

    updated robot container to add all sub-subsystems and controls

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,8 +9,13 @@ import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.subsystems.arm.Arm;
+import frc.robot.subsystems.arm.ElevatorIO;
 import frc.robot.subsystems.arm.ElevatorIOReal;
+import frc.robot.subsystems.arm.IntakeIO;
+import frc.robot.subsystems.arm.IntakeIOReal;
+import frc.robot.subsystems.arm.PivotIO;
 import frc.robot.subsystems.arm.PivotIOReal;
+import frc.robot.subsystems.arm.WristIO;
 import frc.robot.subsystems.arm.WristIOReal;
 
 public class RobotContainer {
@@ -21,9 +26,11 @@ public class RobotContainer {
 
   public RobotContainer() {
     arm = new Arm(
-      new ElevatorIOReal(),
-      new PivotIOReal(),
-      new WristIOReal()
+      new ElevatorIOReal() {},
+      new PivotIOReal() {},
+      new WristIOReal() {},
+      new IntakeIOReal() {}
+
     );
 
     configureBindings();
@@ -31,15 +38,30 @@ public class RobotContainer {
 
   private void configureBindings() {
     // Temporary arm bindings for testing
-    primaryController.povUp().whileTrue(arm.changePivotGoalPosition(15));
-    primaryController.povDown().whileTrue(arm.pivotMoveDown());
+    primaryController.povUp()
+      .whileTrue(arm.changePivotGoalPosition(-15))
+      .onFalse(arm.changePivotGoalPosition(0));
+    primaryController.povDown()
+      .whileTrue(arm.changePivotGoalPosition(15))
+      .onFalse(arm.changePivotGoalPosition(0));;
 
 
     primaryController.rightTrigger(0.25d).whileTrue(arm.elevatorMoveWithVoltage(primaryController.getRightY()));
     primaryController.leftTrigger(0.25d).whileTrue(arm.elevatorMoveWithVoltage(ElevatorConstants.moveVoltage));
 
-    primaryController.rightBumper().whileTrue(arm.wristMoveClockwise());
-    primaryController.leftBumper().whileTrue(arm.wristMoveCounterclockwise());
+    primaryController.rightBumper()
+      .whileTrue(arm.changeWristGoalPosition(15))
+      .onFalse(arm.changeWristGoalPosition(0));
+    primaryController.leftBumper()
+      .whileTrue(arm.changeWristGoalPosition(-15))
+      .onFalse(arm.changeWristGoalPosition(0));
+
+    primaryController.b()
+    .whileTrue(arm.runIntakeCommand(() -> -6))
+    .onFalse(arm.runIntakeCommand(() -> 0));
+    primaryController.y()
+    .whileTrue(arm.runIntakeCommand(() -> 2))
+    .onFalse(arm.runIntakeCommand(() -> 0));;
   }
 
   public Command getAutonomousCommand() {

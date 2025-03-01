3d310d4b089481f1b378be5a2894c0b7dba45062
FlameFish <mccoa499@gmail.com>
2025-03-01 13:54:20 -0800

    Don't initialize AD*, not currently using pathfinding

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,10 +5,8 @@
 package frc.robot;
 
 import com.pathplanner.lib.commands.PathfindingCommand;
-import com.pathplanner.lib.pathfinding.Pathfinding;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.util.LocalADStarAK;
 import org.littletonrobotics.junction.LogFileUtil;
 import org.littletonrobotics.junction.LoggedRobot;
 import org.littletonrobotics.junction.Logger;
@@ -23,8 +21,6 @@ public class Robot extends LoggedRobot {
 
   @Override
   public void robotInit() {
-    Pathfinding.setPathfinder(new LocalADStarAK());
-
     m_robotContainer = new RobotContainer();
 
     // Set up data receivers & replay source

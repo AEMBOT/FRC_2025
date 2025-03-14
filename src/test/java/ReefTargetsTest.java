fdc942b953566e2695aa4bcee5ab6604a0a46187
FlameFish <mccoa499@gmail.com>
2025-03-14 11:52:20 -0700

    give nearest tag tests a small amount of tolerance

--- src/test/java/ReefTargetsTest.java
+++ src/test/java/ReefTargetsTest.java
@@ -3,7 +3,6 @@
 // the WPILib BSD license file in the root directory of this project.
 
 import static frc.robot.constants.VisionConstants.aprilTagFieldLayout;
-import static frc.robot.util.Assertions.assertPoseEquals;
 import static frc.robot.util.Assertions.assertPoseWithin;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -13,11 +12,12 @@ import frc.robot.util.ReefTargets;
 import org.junit.jupiter.api.Test;
 
 public final class ReefTargetsTest {
-  double tolerance = 0.05;
-
   @Test
   void testReefTargetCoralOffset() {
     ReefTargets reefTargetsBlue = new ReefTargets(Alliance.Blue);
+
+    double tolerance = 0.05;
+
     // AprilTag 18
     try {
       assertPoseWithin(
@@ -109,111 +109,124 @@ public final class ReefTargetsTest {
   @Test
   void testTargetBlue() {
     ReefTargets testCase = new ReefTargets(Alliance.Blue);
+    double tolerance = 0.001;
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(18).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(1, 3, new Rotation2d(0))),
-          "(1,3)");
+          "(1,3)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(17).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(0, 0, new Rotation2d(0))),
-          "(0,0)");
+          "(0,0)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(17).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(4, 2, new Rotation2d(0))),
-          "(4,2)");
+          "(4,2)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(22).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(5, 2, new Rotation2d(0))),
-          "(5,2)");
+          "(5,2)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(22).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(7, 1, new Rotation2d(0))),
-          "(7,1)");
+          "(7,1)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(21).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(9, 3, new Rotation2d(0))),
-          "(9,3)");
+          "(9,3)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(21).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(9, 5, new Rotation2d(0))),
-          "(9,5)");
+          "(9,5)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(20).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(8, 7, new Rotation2d(0))),
-          "(8,7)");
+          "(8,7)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(20).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(5.5, 8, new Rotation2d(0))),
-          "(5.5,8)");
+          "(5.5,8)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(19).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(3.5, 8, new Rotation2d(0))),
-          "(3.5,8)");
+          "(3.5,8)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(19).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(2, 7, new Rotation2d(0))),
-          "(2,7)");
+          "(2,7)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(18).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(1, 5, new Rotation2d(0))),
-          "(1,5)");
+          "(1,5)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
@@ -221,59 +234,66 @@ public final class ReefTargetsTest {
 
   @Test
   void testTargetRed() {
-
     ReefTargets testCase = new ReefTargets(Alliance.Red);
 
+    double tolerance = 0.05;
+
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(7).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(15, 4, new Rotation2d(0))),
-          "(15,4)");
+          "(15,4)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(8).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(14, 6, new Rotation2d(0))),
-          "(14,6)");
+          "(14,6)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(9).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(10, 7, new Rotation2d(0))),
-          "(10,7)");
+          "(10,7)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(10).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(7, 4, new Rotation2d(0))),
-          "(7,4)");
+          "(7,4)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(11).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(11, 1, new Rotation2d(0))),
-          "(11,1)");
+          "(11,1)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }
 
     try {
-      assertPoseEquals(
+      assertPoseWithin(
           aprilTagFieldLayout.getTagPose(6).get().toPose2d(),
           testCase.findClosestTagPose(new Pose2d(14, 0, new Rotation2d(0))),
-          "(14,0)");
+          "(14,0)",
+          tolerance);
     } catch (AssertionError e) {
       System.out.println(e.getMessage());
     }

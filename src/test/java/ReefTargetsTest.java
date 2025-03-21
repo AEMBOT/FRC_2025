2926b64f17b001a8816096ea53f87e7cc538cfc8
Ash <114826427+TheFlameFish@users.noreply.github.com>
2025-03-20 17:18:30 -0700

    Minor tweaks made at Chicken Coop (#58)
    * Move reef poses closer
    
    * Log swerve motor temps
    
    * Remove delay in auto intake and place
    
    * Log alignment error
    
    * Adjust reef targets units test

--- src/test/java/ReefTargetsTest.java
+++ src/test/java/ReefTargetsTest.java
@@ -19,61 +19,91 @@ public final class ReefTargetsTest {
     ReefTargets reefTargetsBlue = new ReefTargets(Alliance.Blue);
 
     // AprilTag 18
-    assertPoseWithin(
-        new Pose2d(3.200, 4.205, Rotation2d.fromDegrees(0)),
-        reefTargetsBlue.getReefPose(false, new Pose2d(3.5, 4, new Rotation2d(0)), 4, 0.0),
-        "\n Left L4, (3.5, 4), Coral 0.0, AprilTag 18",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(3.200, 3.705, Rotation2d.fromDegrees(0)),
-        reefTargetsBlue.getReefPose(false, new Pose2d(3.5, 4, new Rotation2d(0)), 4, 0.5),
-        "Left L4, (3.5, 4), Coral 0.5, AprilTag 18",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(3.200, 3.847, Rotation2d.fromDegrees(0)),
-        reefTargetsBlue.getReefPose(true, new Pose2d(3.5, 4, Rotation2d.fromDegrees(0)), 4, 0.0),
-        "Right L4, (3.5, 4), Coral 0.0, AprilTag 18",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(3.200, 3.347, Rotation2d.fromDegrees(0)),
-        reefTargetsBlue.getReefPose(true, new Pose2d(3.5, 4, Rotation2d.fromDegrees(0)), 4, 0.5),
-        "Right L4, (3.5, 4), Coral 0.5, AprilTag 18",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(4.001, 5.231, Rotation2d.fromDegrees(-60)),
-        reefTargetsBlue.getReefPose(false, new Pose2d(4, 4.5, Rotation2d.fromDegrees(-60)), 4, 0.0),
-        "\n Left L4, (4, 4.5), Coral 0.0, AprilTag 19",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(3.568, 4.981, Rotation2d.fromDegrees(-60)),
-        reefTargetsBlue.getReefPose(false, new Pose2d(4, 4.5, Rotation2d.fromDegrees(0)), 4, 0.5),
-        "Left L4, (4, 4.5), Coral 0.5, AprilTag 19",
-        tolerance);
-
+    try {
+      assertPoseWithin(
+          new Pose2d(3.2258, 4.205, Rotation2d.fromDegrees(0)),
+          reefTargetsBlue.getReefPose(false, new Pose2d(3.5, 4, new Rotation2d(0)), 4, 0.0),
+          "\n Left L4, (3.5, 4), Coral 0.0, AprilTag 18",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      assertPoseWithin(
+          new Pose2d(3.2258, 3.705, Rotation2d.fromDegrees(0)),
+          reefTargetsBlue.getReefPose(false, new Pose2d(3.5, 4, new Rotation2d(0)), 4, 0.5),
+          "Left L4, (3.5, 4), Coral 0.5, AprilTag 18",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      assertPoseWithin(
+          new Pose2d(3.2258, 3.847, Rotation2d.fromDegrees(0)),
+          reefTargetsBlue.getReefPose(true, new Pose2d(3.5, 4, Rotation2d.fromDegrees(0)), 4, 0.0),
+          "Right L4, (3.5, 4), Coral 0.0, AprilTag 18",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      assertPoseWithin(
+          new Pose2d(3.2258, 3.347, Rotation2d.fromDegrees(0)),
+          reefTargetsBlue.getReefPose(true, new Pose2d(3.5, 4, Rotation2d.fromDegrees(0)), 4, 0.5),
+          "Right L4, (3.5, 4), Coral 0.5, AprilTag 18",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    // AprilTag 19
+    try {
+      assertPoseWithin(
+          new Pose2d(4.01336013212138, 5.209, Rotation2d.fromDegrees(-60)),
+          reefTargetsBlue.getReefPose(
+              false, new Pose2d(4, 4.5, Rotation2d.fromDegrees(-60)), 4, 0.0),
+          "\n Left L4, (4, 4.5), Coral 0.0, AprilTag 19",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      assertPoseWithin(
+          new Pose2d(3.580347430229161, 4.959, Rotation2d.fromDegrees(-60)),
+          reefTargetsBlue.getReefPose(false, new Pose2d(4, 4.5, Rotation2d.fromDegrees(0)), 4, 0.5),
+          "Left L4, (4, 4.5), Coral 0.5, AprilTag 19",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
     // AprilTag 21
+    try {
+      assertPoseWithin(
+          new Pose2d(5.752846, 4.205, Rotation2d.fromDegrees(180)),
+          reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 4, 0.0),
+          "\n Right L4, (5, 4), Coral 0.0, AprilTag 21",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
+    try {
+      assertPoseWithin(
+          new Pose2d(5.752846, 4.705, Rotation2d.fromDegrees(180)),
+          reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 4, 0.5),
+          "Right L4, (5, 4), Coral 0.5, AprilTag 21",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
 
-    assertPoseWithin(
-        new Pose2d(5.778, 4.205, Rotation2d.fromDegrees(180)),
-        reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 4, 0.0),
-        "\n Right L4, (5, 4), Coral 0.0, AprilTag 21",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(5.778, 4.705, Rotation2d.fromDegrees(180)),
-        reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 4, 0.5),
-        "Right L4, (5, 4), Coral 0.5, AprilTag 21",
-        tolerance);
-
-    assertPoseWithin(
-        new Pose2d(5.778, 4.205, Rotation2d.fromDegrees(180)),
-        reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 1, 0.5),
-        "\n Right L1, (5, 4), Coral 0.0, AprilTag 21",
-        tolerance);
+    try {
+      assertPoseWithin(
+          new Pose2d(5.752, 4.205, Rotation2d.fromDegrees(180)),
+          reefTargetsBlue.getReefPose(true, new Pose2d(5, 4, Rotation2d.fromDegrees(0)), 1, 0.5),
+          "\n Right L1, (5, 4), Coral 0.0, AprilTag 21",
+          tolerance);
+    } catch (AssertionError e) {
+      System.out.println(e.getMessage());
+    }
   }
 
   @Test

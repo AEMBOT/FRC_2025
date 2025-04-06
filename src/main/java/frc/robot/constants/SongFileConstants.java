e68a021eb65f953b8145a2618bc4210fd5906bf2
notpro-ek <kimeli5649@gmail.com>
2025-04-05 22:25:02 -0700

    constant fix

--- src/main/java/frc/robot/constants/SongFileConstants.java
+++ src/main/java/frc/robot/constants/SongFileConstants.java
@@ -1,10 +1,8 @@
 package frc.robot.constants;
 
 public class SongFileConstants {
-  public static final String song1_calm1 =
-      "C:\\Users\\kimel\\FRC-robotics\\FRC_2025\\src\\main\\deploy\\songs\\calm_1Chrp.chrp";
-  public static final String song2_pigstep =
-      "C:\\Users\\kimel\\FRC-robotics\\FRC_2025\\src\\main\\deploy\\songs\\pigstep9tracks.chrp";
+  public static final String song1_calm1 = "calm_1Chrp.chrp";
+  public static final String song2_pigstep = "pigstep9tracks.chrp";
   public static final String song3_ = "";
   public static final String song4_ = "";
   public static final String song5_ = "";

cf25fdb35d9da2d12b13524a117bebc5722ee58f
FlameFish <mccoa499@gmail.com>
2025-04-29 17:18:36 -0700

    fix song paths

--- src/main/java/frc/robot/constants/SongFileConstants.java
+++ src/main/java/frc/robot/constants/SongFileConstants.java
@@ -1,8 +1,8 @@
 package frc.robot.constants;
 
 public class SongFileConstants {
-  public static final String song1_calm1 = "calm_1Chrp.chrp";
-  public static final String song2_pigstep = "pigstep9tracks.chrp";
+  public static final String song1_calm1 = "/home/lvuser/deploy/songs/calm_1Chrp.chrp";
+  public static final String song2_pigstep = "/home/lvuser/deploy/songs/pigstep9tracks.chrp";
   public static final String song3_ = "";
   public static final String song4_ = "";
   public static final String song5_ = "";

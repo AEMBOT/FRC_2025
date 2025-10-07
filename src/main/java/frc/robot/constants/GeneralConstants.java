c364f7f75fcb95f629837500368837350576221b
FlameFish <mccoa499@gmail.com>
2025-10-07 12:23:24 -0700

    Spotless

--- src/main/java/frc/robot/constants/GeneralConstants.java
+++ src/main/java/frc/robot/constants/GeneralConstants.java
@@ -47,5 +47,6 @@ public class GeneralConstants {
    * The minimum space available on the log USB drive. If this space is not available, logs will be
    * deleted on init until there is space available.
    */
-  public static final long LOG_SPACE_REQUIREMENT = (long) (0.05 * 1073741824); // Convert Gb to Bytes
+  public static final long LOG_SPACE_REQUIREMENT =
+      (long) (0.05 * 1073741824); // Convert Gb to Bytes
 }
